1ae07bbb100e840f21249d15bcb6638a378393cb
davidtobe13 <davidtobe1999@gmail.com>
2024-09-02 11:16:30 +0100

    neeww

---
+++ javascript/ninja.js
@@ -0,0 +1,54 @@
+// //DOM Manipulation
+// let headers = document.querySelector(".h3");
+// // headers.innerText = " Header";
+// headers.innerHTML = "<p>Hello world</p>";
+
+// let divTag = document.querySelector("div");
+// divTag.innerHTML += "<p>This is a paragraph</p>";
+// divTag.innerHTML += "<button>Click Me</button>";
+
+// let time2 = "2*1=2, 2*2=4, 2*3=6, 2*4=8, 2*5=10, 2*6=12, 2*7=14, 2*8=16, 2*9=18, 2*10=20, 2*11=22, 2*12=24"
+// let time3 = "3*1=3, 3*2=6, 3*3=9, 3*4=12, 3*5=15, 3*6=18, 3*7=21, 3*8=24, 4*9=27, 3*10=30, 3*11=33, 3*12=36"
+// let time4 = "4*1=4, 4*2=8, 4*3=12, 4*4=16, 4*5=20, 4*6=24, 4*7=28, 4*8=32, 4*9=36, 4*10=40, 4*11=44, 4*12=48"
+
+//     console.log(time2);
+    
+// for
+
+//function multiplynum2(){
+    // for(i = 0; i < 13; i++){
+    //     console.log(2 + " * " + i + " = " + 2*i)
+    // }
+
+//}
+
+
+//multiplynum2()
+
+// function multiplyNum(){
+//     let myNumbers = 1
+
+//     for(i = 0; i < 13; i++){
+
+//         console.log(3 + " * " + i + " = " + 3*i)
+//     }
+// }
+
+// multiplyNum();
+
+//2times multiplication
+for(i = 1; i <= 12; i++){
+    console.log("i = " + i);
+    
+    console.log(i*2);
+}
+
+// 3times multiplication 
+for(i = 1; i >= 12; i++){
+    console.log("i = " + i);
+    
+    let operation = 3 * i;
+    console.log(operation);
+}
+
+
---
+++ javascript/script.js
@@ -0,0 +1,148 @@
+// let addNumber1 = 2;
+// let addNumber2 = 10;
+// let numbers = null;
+
+// document.getElementById('test').addEventListener('click', addalpha)
+
+// function addnumber(){
+//     number = addNumber1 + addNumber2;
+    
+//     console.log(number)
+// }
+
+// let addstring = "i know how to code";
+// let addNumber = 100;
+// let answer = null;
+
+// function addalpha(){
+//     answer = addstring + addNumber;
+
+//     console.log(anwser)
+// }
+
+// let testArray = ["chisom", " favour", " pentecost"];
+
+// console.log(testArray.join(","));
+
+// //Template strings
+// let isAllowed = true;
+
+// //Concatenation of strings
+// console.log("You are allowed because this -> " + isAllowed + " is true");
+
+
+// //Conditionals
+// let number = 4; //declaration of variable
+
+// if(number === 2){
+//     console.log("Number is equal to 2");
+// }
+// else if(number === 4 || number === 9){
+//     console.log("Number is equal to " + number);
+// }
+// else{
+//     console.log("Number is equal to " + number);   
+// }
+
+// // DOM Manipulation
+// //Query Selectors
+// function multiplyNum(){
+//     let myNumbers = 1;
+
+//     let theNumber = prompt('enter a number to check 😉')
+
+//     for(i = 0; i < 13; i++){
+//         alert(theNumber + " * " + i + " = " + theNumber*i)
+//     }
+// }
+// multiplyNum()
+
+
+
+
+// function assg(){
+//     let thisString = "i love javascript";
+
+//     for(i = 0; i < 56; i++){
+//         console.log(thisString)
+//     }
+// }
+
+// assg()
+
+
+// function assg2(){
+//     let thisString2 = "html is all about the structure";
+
+//     for(i = 0; i < 20; i++){
+//         console.log(thisString2)
+//     }
+// }
+
+// assg2()
+
+// function assg3(){
+//     let thisString3 = "css is all about styling";
+
+//     for(i = 0; i < 100; i++)
+//         console.log(thisString3)
+// }
+
+// assg3()
+
+// function magic() {
+//     let card = 4;
+
+//     for(i = 0; i < 13; i++){
+//         console.log(card + " * " + i + " = " + card*i)
+//     }
+// }
+
+// magic()
+
+function classes() {
+    let thearray = [2, 4, 6, 8, 10]
+
+    let mappedNumbers = thearray.map(T=>T+2)
+
+    console.log(mappedNumbers)
+        
+
+}
+
+classes()
+
+function work() {
+    let thework = [2, 4, 6, 8, 10]
+
+    let allmappednumbers = thework.map(O=>O*2)
+    
+    console.log(allmappednumbers);
+    
+}
+
+work()
+
+function work2() {
+    let thework2 = [2, 4, 6, 8, 10]
+
+    let allmappednumbers1 = thework2.map(B=>B/2)
+
+    console.log(allmappednumbers1);
+    
+}
+
+work2()
+
+function work3(
+    
+) {
+    let thework3 = [2, 4, 6, 8, 10]
+
+    let allmappednumbers2 = thework3.map(E=>E-2)
+
+    console.log(allmappednumbers2);
+    
+}
+
+work3()
